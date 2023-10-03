5111971539d8dc9fc2404521db0ae70b5f63be6c
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-10-03 22:03:33 +0530

    [Shantanu]: generics code is added

---
+++ Minimum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Generics
+{
+    public class Minimum
+    {
+        public T FindMinimum<T>(T value1, T value2, T value3) where T : IComparable<T>
+        {
+            T min = value1;
+
+            if (value2.CompareTo(min) < 0)
+                min = value2;
+
+            if (value3.CompareTo(min) < 0)
+                min = value3;
+
+            return min;
+        }
+
+        
+    }
+
+
+    public class MinimumFinder<T> where T : IComparable<T>
+    {
+        private T value1;
+        private T value2;
+        private T value3;
+
+        public MinimumFinder(T value1, T value2, T value3)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
+        }
+
+        public T FindMinimum()
+        {
+            return Minimum(value1, value2, value3);
+        }
+
+        private T Minimum(T v1, T v2, T v3)
+        {
+            T min = v1;
+
+            if (v2.CompareTo(min) < 0)
+                min = v2;
+
+            if (v3.CompareTo(min) < 0)
+                min = v3;
+
+            return min;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 5;
+            int num3 = 8;                      
+            int minInt = Math.Min(Math.Min(num1, num2), num3);
+            Console.WriteLine("Minimum Integer: " + minInt);
+
+            float num4 = 10.5f;
+            float num5 = 5.2f;
+            float num6 = 8.7f;
+            float minFloat = Math.Min(Math.Min(num4, num5), num6);
+            Console.WriteLine("Minimum Float: " + minFloat);
+
+
+            string str1 = "apple";
+            string str2 = "banana";
+            string str3 = "cherry";
+            string minString = str1;
+
+            if (string.Compare(str2, minString) < 0)
+                minString = str2;
+
+            if (string.Compare(str3, minString) < 0)
+                minString = str3;
+
+            Console.WriteLine("Minimum String: " + minString);
+
+
+            
+            Minimum min = new Minimum();
+            Console.WriteLine(min.FindMinimum<int>(4,3,2));
+            Console.WriteLine(min.FindMinimum<float>(2.1f,3.2f,4.5f));
+            Console.WriteLine(min.FindMinimum<string>("apple","banana","cherry"));
+            
+            
+            MinimumFinder<int> intFinder = new MinimumFinder<int>(10, 5, 8);
+            int minInt1 = intFinder.FindMinimum();
+            Console.WriteLine("Minimum Integer: " + minInt1);
+
+            MinimumFinder<float> floatFinder = new MinimumFinder<float>(10.5f, 5.2f, 8.7f);
+            float minFloat1 = floatFinder.FindMinimum();
+            Console.WriteLine("Minimum Float: " + minFloat1);
+
+            MinimumFinder<string> stringFinder = new MinimumFinder<string>("apple", "banana", "cherry");
+            string minString1 = stringFinder.FindMinimum();
+            Console.WriteLine("Minimum String: " + minString1);
+        }
+    }
+}
